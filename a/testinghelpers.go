d1de50671ccf149c59f74c4ebdfa749d9c3be8a6
Jack <jack@signalfuse.com>
2014-10-10 15:25:18 -0700

    test helpers added

---
+++ a/testinghelpers.go
@@ -0,0 +1,35 @@
+package a
+
+import (
+	"strings"
+	"testing"
+	"runtime"
+)
+
+func previousStack() string {
+	buf := make([]byte, 10000)
+	runtime.Stack(buf, false)
+	return string(buf)
+}
+
+func ExpectEquals(t *testing.T, item interface{}, equals interface{}, msg string) {
+	if item != equals {
+		t.Errorf("%s != %s: %s @ %s", item, equals, msg, previousStack())
+	}
+}
+
+func ExpectNotEquals(t *testing.T, item interface{}, equals interface{}, msg string) {
+	if item == equals {
+		buf := make([]byte, 10000)
+		runtime.Stack(buf, false)
+		t.Errorf("%s != %s: %s @ %s", item, equals, msg, previousStack())
+	}
+}
+
+func ExpectContains(t *testing.T, item string, substr string, msg string) {
+	if !strings.Contains(item, substr) {
+		buf := make([]byte, 10000)
+		runtime.Stack(buf, false)
+		t.Errorf("%s DOES NOT CONTAIN %s: %s @ %s", item, substr, msg, previousStack())
+	}
+}
